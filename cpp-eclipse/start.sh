7bb9afcaf52cfd5f5eb488c0b7913ed9fdd28279
shawks03 <shawks03@yahoo.com>
2017-12-06 16:35:28 -0500

    Cleanup on stop. Removed privileged flag as user should work with existing permissions in volume.

--- cpp-eclipse/start.sh
+++ cpp-eclipse/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --privileged -d -p 127.0.0.1:2222:22 -v $HOME/workspace:/home/$USER/workspace:rw applications/cpp-eclipse
+docker run --rm -d -p 127.0.0.1:2222:22 -v $HOME/workspace:/home/$USER/workspace:rw applications/cpp-eclipse
